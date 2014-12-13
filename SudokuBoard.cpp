972c1bf68f0b21031582d42b2fbbb243be0f98fe
Alfred <alfred-zb@hotmail.com>
2014-12-13 23:14:39 +0100

    Made constructors for Sudokuboard

--- SudokuBoard.cpp
+++ SudokuBoard.cpp
@@ -6,13 +6,72 @@
  */
 
 #include "SudokuBoard.h"
+using namespace std;
 
-SudokuBoard::SudokuBoard() {
-	// TODO Auto-generated constructor stub
+/******** Constructors *********/
+
+SudokuBoard::SudokuBoard(const SudokuBoard& sb) 
+{
+  
+  *this.board.resize(9);
+  for(unsigned i = 0; i < 9; i++)
+    {
+      *this.board[i].resize(9);
+      for(unsigned j = 0; j < 9; j++)
+	{
+	  *this.board[i][j].resize(sb.board[i][j].size());
+	}
+    }
+    
+  for(unsigned i = 0; i < 9; i++)
+    {
+      for(unsigned j = 0; j < 9; j++)
+	{
+	  for(unsigned k = 0; k < sb.board[i][j].size(); k++)
+	    {
+	      *this.board[i][j][k] = sb.board[i][j][k];
+	    }
+	}
+    }
+
+  *this.section.resize(9);
+  for(unsigned i = 0; i < 9; i++)
+    {
+      *this.section[i].resize(9);
+   }
+  
+   for(unsigned i = 0; i < 9; i++)
+    {
+      for(unsigned j = 0; j < 9; j++)
+	{
+	  *this.section[i][j] = sb.section[i][j];
+	}
+    } 
 
 }
 
-SudokuBoard::~SudokuBoard() {
-	// TODO Auto-generated destructor stub
-}
 
+SudokuBoard::SudokuBoard(vector<vector<unsigned>> initialBoard)
+{
+   *this.board.resize(9);
+  for(unsigned i = 0; i < 9; i++)
+    {
+      *this.board[i].resize(9);
+    }
+  
+  
+  *this.section.resize(9);
+  for(unsigned i = 0; i < 9; i++)
+    {
+      *this.section[i].resize(9);
+   }
+  
+  for(unsigned i = 0; i < 9; i++)
+    {
+      for(unsigned j = 0; j < 9; j++)
+	{
+	  *this.board[i][j][0] = initialBoard[i][j]; 
+	}
+    }
+  
+}
--- SudokuBoard.h
+++ SudokuBoard.h
@@ -9,9 +9,18 @@
 #define SUDOKUBOARD_H_
 
 class SudokuBoard {
-public:
-	SudokuBoard();
-	virtual ~SudokuBoard();
+ private:
+  SudokuBoard() = delete;
+  SudokuBoard(const SudokuBoard&);
+  SudokuBoard(SudokuBoard&&) = delete;
+  SudokuBoard(std::array<unsigned>);
+  ~SudokuBoard() = default;
+  std::vector<vector<vector<unsigned>>> board; //3D board with possible values in the 3rd dimension
+  std::vector<vector<unsigned>> section;
+ public:
+  std::vector<unsigned> checkPossibleValues(const unsigned,const unsigned); //returns possible values for a square
+  void setPossibleValues(const unsigned, const unsigned);
+  void setAllPossibleValues();
 };
 
 #endif /* SUDOKUBOARD_H_ */
