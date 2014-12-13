b71c233c2e13fb530505709bdf4c3fb52498e664
Adam Gudjonsson, Development manager <adagu229@student.liu.se>
2014-12-13 18:38:58 +0100

    Added the SudokuBoard files

---
+++ SudokuBoard.cpp
@@ -0,0 +1,18 @@
+/*
+ * SudokuBoard.cpp
+ *
+ *  Created on: 13 dec 2014
+ *      Author: Adam
+ */
+
+#include "SudokuBoard.h"
+
+SudokuBoard::SudokuBoard() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SudokuBoard::~SudokuBoard() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ SudokuBoard.h
@@ -0,0 +1,17 @@
+/*
+ * SudokuBoard.h
+ *
+ *  Created on: 13 dec 2014
+ *      Author: Adam
+ */
+
+#ifndef SUDOKUBOARD_H_
+#define SUDOKUBOARD_H_
+
+class SudokuBoard {
+public:
+	SudokuBoard();
+	virtual ~SudokuBoard();
+};
+
+#endif /* SUDOKUBOARD_H_ */
